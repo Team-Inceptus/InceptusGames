1314affbb019970c990fc947b1e3174fdc0c0593
GamerCoder215 <gamercoder215@gmail.com>
2022-05-14 19:35:26 -0500

    Update javadoc.sh

--- javadoc.sh
+++ javadoc.sh
@@ -3,49 +3,35 @@
 git config user.name github-actions[bot]
 git config user.email 41898282+github-actions[bot]@users.noreply.github.com
 git fetch origin gh-pages
-git reset --hard
 git checkout origin/gh-pages
-
-rm -rf .
-
-if [ ! -d "docs" ]; then
-  mkdir docs
-fi;
 
 for dir in ./*
 do
-  rm -rf "./docs/${dir/.\//}"
+  rm -rf "$dir"
 done
 
 for dir in ./*
 do
-  if [ "$dir" == "./docs" ]; then
-    continue
-  fi
-
   if [ -d "$dir" ]; then
-    if [ ! -f "./docs/$dir" ]
+    if [ ! -f "$dir" ]
     then
-        mkdir "./docs/${dir/.\//}"
+        mkdir "$dir"
     fi
-    cp -Rfv "$dir"/target/apidocs/* ./docs/"${dir/.\//}"/
+    cp -Rfv "$dir"/target/apidocs/* "$dir"/
   fi
 done
 
 for dir in ./premium/*
 do
   if [ -d "$dir" ]; then
-    if [ ! -f "./docs/$dir" ]
+    if [ ! -f "$dir" ]
     then
-        mkdir "./docs/${dir/.\//}"
+        mkdir "$dir"
     fi
-    cp -Rfv "$dir"/target/apidocs/* ./docs/"${dir/.\//}"/
+    cp -Rfv "$dir"/target/apidocs/* "$dir"/
   fi
 done
 
-mv ./docs/* .
 git add .
 git commit -m "Update InceptusGames JavaDocs"
-git branch -D gh-pages
-git branch -m gh-pages
 git push -f origin gh-pages
