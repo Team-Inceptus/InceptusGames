b5d07dd2cd3a0e751ceb0d51cf44b68ebb23d236
GamerCoder215 <gamercoder215@gmail.com>
2022-05-14 19:43:48 -0500

    Update javadoc.sh

--- javadoc.sh
+++ javadoc.sh
@@ -3,34 +3,61 @@
 git config user.name github-actions[bot]
 git config user.email 41898282+github-actions[bot]@users.noreply.github.com
 git fetch origin gh-pages
-git checkout origin/gh-pages
+
+if [ ! -d "docs" ]; then
+  mkdir docs
+fi;
+
+echo Creating Docs...
 
 for dir in ./*
 do
-  rm -rf "$dir"
+  rm -rf "./docs/${dir/.\//}"
 done
 
 for dir in ./*
 do
+  if [ "$dir" == "./docs" ]; then
+    continue
+  fi
+
   if [ -d "$dir" ]; then
-    if [ ! -f "$dir" ]
+    if [ ! -f "./docs/$dir" ]
     then
-        mkdir "$dir"
+        mkdir "./docs/${dir/.\//}"
     fi
-    cp -Rfv "$dir"/target/apidocs/* "$dir"/
+    cp -Rfv "$dir"/target/apidocs/* ./docs/"${dir/.\//}"/
   fi
 done
 
 for dir in ./premium/*
 do
   if [ -d "$dir" ]; then
-    if [ ! -f "$dir" ]
+    if [ ! -f "./docs/$dir" ]
     then
-        mkdir "$dir"
+        mkdir "./docs/${dir/.\//}"
     fi
-    cp -Rfv "$dir"/target/apidocs/* "$dir"/
+    cp -Rfv "$dir"/target/apidocs/* ./docs/"${dir/.\//}"/
   fi
 done
+
+echo "Moving Docs (1)..."
+
+git checkout origin/gh-pages
+
+for dir in ./*
+do
+  if [ "$dir" == "./docs" ]; then
+    continue
+  fi
+
+  rm -rf "$dir"
+done
+
+echo "Moving Docs (2)..."
+
+cp -Rfv ./docs/* ./
+rm -rf ./docs
 
 git add .
 git commit -m "Update InceptusGames JavaDocs"
